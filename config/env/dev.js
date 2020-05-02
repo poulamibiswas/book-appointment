0d6870eeef69242a5c643f7ef5eaecc21084e157
Poulami Biswas <poulami@thoughtworks.com>
2020-05-02 19:02:47 +0530

    adds dummy dev.js to store all config related to sending emails

---
+++ config/env/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+  clientId: "<insert client id here>",
+  clientSecret: "<insert client secret here>",
+  redirectUrl: "https://developers.google.com/oauthplayground", // Redirect URL
+  refreshToken: "<insert refresh token here>",
+  emailUser:
+    "<insert email user with which you have setup google api acc here>",
+  adminEmail: "<insert admin email here>",
+};
